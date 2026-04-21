087a4f29cb4670ee770e722692a2586ab76f7e0b
anduurii <karlandri2006@hotmail.com>
2026-04-21 10:37:00 +0000

    server komið og index að virka

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'test'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
